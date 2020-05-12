8780c397a817dd0f28adee8cd9e64339194fdc18
JohnLi94 <65105969+JohnLi94@users.noreply.github.com>
2020-05-12 10:51:47 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+//import Home from './components/Home';
+
+import News from './components/News';
+
+function App() {
+  return (
+    <div className="App">
+      这里是根组件啊！
+
+      {/* <Home/> */}
+      <News/>
+
+    </div>
+  );
+}
+
+export default App;
